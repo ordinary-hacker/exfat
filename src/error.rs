3c17abfdf3a328bf4b780315290ffa654295ad35
Ordinary Hacker <ordinary_hacker@proton.me>
2025-10-26 17:11:31 -0600

    Removed diplaydoc usage which requires std

--- src/error.rs
+++ src/error.rs
@@ -1,76 +1,110 @@
-use displaydoc::Display;
 use thiserror::Error;
 
-#[derive(Debug, Display)]
+#[derive(Debug)]
 pub enum DataError {
-    /// Not exFAT filesystem
     NotExFAT,
-    /// Bad boot sector checksum
     BootChecksum,
-    /// Allocation bitmap missing
     AllocationBitmapMissing,
-    /// Upcase table missing
     UpcaseTableMissing,
-    /// Bad upcase table checksum
     UpcaseTableChecksum,
-    /// Broken FAT chain
     FATChain,
-    /// Broken file or directory metadata
     Metadata,
+}
+
+impl core::fmt::Display for DataError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            DataError::NotExFAT => write!(f, "Not exFAT filesystem"),
+            DataError::BootChecksum => write!(f, "Bad boot sector checksum"),
+            DataError::AllocationBitmapMissing => write!(f, "Allocation bitmap missing"),
+            DataError::UpcaseTableMissing => write!(f, "Upcase table missing"),
+            DataError::UpcaseTableChecksum => write!(f, "Bad upcase table checksum"),
+            DataError::FATChain => write!(f, "Broken FAT chain"),
+            DataError::Metadata => write!(f, "Broken file or directory metadata"),
+        }
+    }
 }
 
 impl core::error::Error for DataError {}
 
-#[derive(Debug, Display)]
+#[derive(Debug)]
 pub enum ImplementationError {
-    /// TexFAT not supported
     TexFATNotSupported,
-    /// Create directory not supported
     CreateDirectoryNotSupported,
+}
+
+impl core::fmt::Display for ImplementationError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            ImplementationError::TexFATNotSupported => write!(f, "TexFAT not supported"),
+            ImplementationError::CreateDirectoryNotSupported => write!(f, "Create directory not supported"),
+        }
+    }
 }
 
 impl core::error::Error for ImplementationError {}
 
-#[derive(Debug, Display)]
+#[derive(Debug)]
 pub enum InputError {
-    /// Name too long
     NameTooLong,
-    /// Seek position out of range
     SeekPosition,
-    /// Size out of range
     Size,
+}
+
+impl core::fmt::Display for InputError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            InputError::NameTooLong => write!(f, "Name too long"),
+            InputError::SeekPosition => write!(f, "Seek position out of range"),
+            InputError::Size => write!(f, "Size out of range"),
+        }
+    }
 }
 
 impl core::error::Error for InputError {}
 
-#[derive(Debug, Display)]
+#[derive(Debug)]
 pub enum AllocationError {
-    /// Allocation-not-possible is set in file metadata
     NotPossible,
-    /// Need fragment while dont-fragment is set in file options
     Fragment,
-    /// No more cluster available
     NoMoreCluster,
+}
+
+impl core::fmt::Display for AllocationError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            AllocationError::NotPossible => write!(f, "Allocation-not-possible is set in file metadata"),
+            AllocationError::Fragment => write!(f, "Need fragment while dont-fragment is set in file options"),
+            AllocationError::NoMoreCluster => write!(f, "No more cluster available"),
+        }
+    }
 }
 
 impl core::error::Error for AllocationError {}
 
-#[derive(Debug, Display)]
+#[derive(Debug)]
 pub enum OperationError {
-    /// File or directory already open
     AlreadyOpen,
-    /// File or directory not found
     NotFound,
-    /// Not a file
     NotFile,
-    /// Not a directory
     NotDirectory,
-    /// File or directory already exists
     AlreadyExists,
-    /// Directory not empty when deleting
     DirectoryNotEmpty,
-    /// End of file
     EOF,
+}
+
+impl core::fmt::Display for OperationError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            OperationError::AlreadyOpen => write!(f, "File or directory already open"),
+            OperationError::NotFound => write!(f, "File or directory not found"),
+            OperationError::NotFile => write!(f, "Not a file"),
+            OperationError::NotDirectory => write!(f, "Not a directory"),
+            OperationError::AlreadyExists => write!(f, "File or directory already exists"),
+            OperationError::DirectoryNotEmpty => write!(f, "Directory not empty when deleting"),
+            OperationError::EOF => write!(f, "End of file"),
+        }
+    }
 }
 
 impl core::error::Error for OperationError {}
